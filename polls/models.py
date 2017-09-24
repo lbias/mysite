f0f4807905b77381370ffd93cbf9f062dd086606
lbias <306508354@qq.com>
2017-09-24 23:39:50 +0800

    add a __str__() method to both model question and choice

--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
+@python_2_unicode_compatible  # only if you need to support Python 2
 class Question(models.Model):
+    def __str__(self):
+        return self.question_text
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+@python_2_unicode_compatible  # only if you need to support Python 2
 class Choice(models.Model):
+    def __str__(self):
+        return self.choice_text
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
